d046314f374fd5f920f20604a086cc9c1f72dba6
Cowbon <alexalex850520@gmail.com>
2020-11-16 00:50:23 +0800

    create db command

--- flask-test.py
+++ flask-test.py
@@ -9,3 +9,10 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@app.cli.command()
+def create_db(): # with command 'flask create-db'
+    print('create db')
+    from app import db
+    db.create_all()
